0a1cf6eea759f46055b08d6f27a2c0137598e551
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-06-26 22:52:15 +0200

    Feat: added layer table of attributes

--- components/map/layerList.js
+++ components/map/layerList.js
@@ -1,16 +1,72 @@
-import {List , ListItem,ListItemText} from "@mui/material"
+import { List, ListItem, ListItemText, Dialog, DialogContent, TableContainer, Table, TableHead, Paper, TableRow, TableCell, Typography, IconButton } from "@mui/material"
+import { useState } from "react"
+import { BiTable } from "react-icons/bi"
 export default function LayerList(props) {
-  return (
-    <List>
-        {
-            props.layers.map((layer,i)=>{
-                return(
-                    <ListItem key={i}>
-                        <ListItemText primary={layer.name} secondary={layer.features.length+" Features"}/>
-                    </ListItem>
-                )
-            })
-        }
-    </List>
-  )
+    const [tableOpen, setTableOpen] = useState(false)
+    return (
+        <List>
+            {
+                props.layers.map((layer, i) => {
+                    return (
+                        <ListItem key={i} sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <ListItemText primary={layer.name} secondary={layer.features.length + " Features"}  />
+                            <IconButton color="primary" onClick={() => {
+                                setTableOpen(true)
+                            }}>
+                                <BiTable title="Layer Table" size={30} />
+                            </IconButton>
+                            
+
+
+
+                            <Dialog open={tableOpen} maxWidth onClose={(e) => { setTableOpen(false) }}>
+                                <DialogContent>
+                                    <TableContainer component={Paper}>
+                                        <Table aria-label="layer table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    {
+
+                                                        Object.keys(layer.features[0].properties).map((layerProperties, i) => {
+
+                                                            return (
+                                                                <TableCell key={i}>
+                                                                    {layerProperties}
+                                                                </TableCell>
+                                                            )
+                                                        })
+                                                    }
+                                                </TableRow>
+                                            </TableHead>
+
+                                            {
+                                                layer.features.map((ft, i) => {
+                                                    console.log(typeof (ft.properties));
+                                                    return (
+                                                        <TableRow key={i}>
+                                                            {
+                                                                Object.values(ft.properties).map((prop, i) => {
+                                                                    return (
+                                                                        <TableCell key={i}>
+                                                                            {prop}
+                                                                        </TableCell>
+                                                                    )
+                                                                })
+                                                            }
+
+                                                        </TableRow>
+                                                    )
+                                                })
+                                            }
+
+                                        </Table>
+                                    </TableContainer>
+                                </DialogContent>
+                            </Dialog>
+                        </ListItem>
+                    )
+                })
+            }
+        </List >
+    )
 }
