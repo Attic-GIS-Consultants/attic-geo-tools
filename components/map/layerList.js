1a7386b32c533f8c2d964cd91091aab3d3beb732
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-06-27 14:12:16 +0200

    Feat: added layer symbol and color in layer list

--- components/map/layerList.js
+++ components/map/layerList.js
@@ -1,6 +1,7 @@
 import { Checkbox,List, ListItem, ListItemText, Dialog, DialogContent, TableContainer, Table, TableHead, Paper, TableRow, TableCell, Typography, IconButton } from "@mui/material"
 import { useState } from "react"
-import { BiTable } from "react-icons/bi"
+import { BiTable , } from "react-icons/bi"
+import {BsDot , BsSlashLg,BsSquareFill} from "react-icons/bs"
 export default function LayerList(props) {
     const [tableOpen, setTableOpen] = useState(false)
     return (
@@ -8,8 +9,8 @@ export default function LayerList(props) {
             {
                 props.layers.map((layer, i) => {
                     return (
-                        <ListItem key={i} sx={{ display: 'flex', flexDirection: 'column' }}>
-                            <Checkbox  defaultChecked onChange={(e)=>{
+                        <ListItem key={i} sx={{ display: 'flex', gap:2}}>
+                            <Checkbox title="Turn off"  defaultChecked onChange={(e)=>{
                                 if (e.target.checked == false){
                                     const filteredLayers = props.layers.filter(tempLayer => tempLayer.name != layer.name)
                                     props.setLayers(filteredLayers)
@@ -17,11 +18,12 @@ export default function LayerList(props) {
                                     props.setLayers([...props.layers,layer])
                                 } 
                             }} />
+                            <LayerSymbol color={layer.style.color} geomType={layer.features[0].geometry.type}/>
                             <ListItemText primary={layer.name} secondary={layer.features.length + " Features"}  />
                             <IconButton color="primary" onClick={() => {
                                 setTableOpen(true)
                             }}>
-                                <BiTable title="Layer Table" size={30} />
+                                <BiTable title="Attributes Table" size={30} />
                             </IconButton>
                             <Dialog open={tableOpen} maxWidth onClose={(e) => { setTableOpen(false) }}>
                                 <DialogContent>
@@ -43,7 +45,6 @@ export default function LayerList(props) {
                                             </TableHead>
                                             {
                                                 layer.features.map((ft, i) => {
-                                                    console.log(typeof (ft.properties));
                                                     return (
                                                         <TableRow key={i}>
                                                             {
@@ -67,6 +68,19 @@ export default function LayerList(props) {
                     )
                 })
             }
-        </List >
+        </List>
     )
+}
+
+function LayerSymbol(props){
+    if (props.geomType == "Point" || props.geomType == "Multi-Point" ){
+        return <BsDot size={30} color={props.color} />
+    }
+    else if (props.geomType == "Line" || props.geomType == "Multi-Line" || props.geomType == "LineString" ) {
+        return <BsSlashLg size={30} color={props.color} />
+    }
+    if (props.geomType == "Polygon" || props.geomType == "Multi-Polygon") {
+        return <BsSquareFill size={30} color={props.color} />
+    }
+    
 }
