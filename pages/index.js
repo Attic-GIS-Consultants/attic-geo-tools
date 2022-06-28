895dde869458f0543f234fa4319dfbd9d168f184
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-06-28 23:27:49 +0200

    Fix: lorem ipsum

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
         <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
           <Typography variant="h2">Our Philosophy</Typography>
           <Box sx={{marginTop:2}}>
-            <Typography sx={{lineHeight:'45px', fontWeight:400}} variant="body1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
+            <Typography sx={{lineHeight:'45px', fontWeight:400}} variant="body1">{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}</Typography>
           </Box>
         </Container>
       </main>
