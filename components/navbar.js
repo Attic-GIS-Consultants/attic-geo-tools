1fcd355d252024e5d3094399ccb7ac0b22b4609a
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-07-10 19:20:23 +0200

    Fix: added div to wrap hamburger

--- components/navbar.js
+++ components/navbar.js
@@ -4,19 +4,22 @@ import React, { useState } from 'react'
 import { VscThreeBars, VscClose } from "react-icons/vsc"
 import styles from "../styles/navbar.module.css"
 export default function Navbar() {
-  const [barClicked,setBarClicked] = useState(false)
+  const [barClicked, setBarClicked] = useState(false)
   return (
     <nav className={styles.nav}>
       <div className={styles.navBar}>
-        <Typography variant='h4' sx={{ fontSize: '20pt',marginTop:1,marginLeft:1, fontWeight:500}}>
+        <Typography variant='h4' sx={{ fontSize: '20pt', marginTop: 1, marginLeft: 1, fontWeight: 500 }}>
           ATTIC GEO TOOLS
         </Typography>
-        <IconButton color="primary" className={styles.hamBurger} sx={{}} onClick={()=>{
-          setBarClicked(!barClicked)
-        }}>
-          {barClicked ? <VscClose size={30} /> : <VscThreeBars size={30} /> }
-          
-        </IconButton>
+        <div className={styles.hamBurger}>
+          <IconButton color="primary" sx={{}} onClick={() => {
+            setBarClicked(!barClicked)
+          }}>
+            {barClicked ? <VscClose size={30} /> : <VscThreeBars size={30} />}
+
+          </IconButton>
+        </div>
+
       </div>
       <div className={barClicked ? styles.navShow : styles.navHidden}>
         <Link href="/"><a><Typography variant="body1">Home</Typography></a></Link>
